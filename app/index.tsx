debb815d65adf38b08f56cde4fd50335223dbbb1
Alexander_Ovchar <azalexxx02@gmail.com>
2024-09-16 13:01:17 +0300

    add assets

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, ImageBackground} from "react-native";
+import React from "react";
+
+import beachBg from "@/assets/meditation-images/beach.webp";
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachBg}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <Text>App</Text>
+      </ImageBackground>
     </View>
-  )
+  );
 }
